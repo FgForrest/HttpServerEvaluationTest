c888d2e80f8942e1719ee3297421e5daa31523c8
Jan Novotný <novotnaci@gmail.com>
2023-04-07 11:05:12 +0200

    Implemented Francesco Nigro recommendations:
    - moved GraphQL server to asynchronous thread
    - eliminated connection closing that was not done in different implementations
    
    Benchmark                                                    Mode  Cnt      Score      Error  Units
    ServersThroughputBenchmark.graphQLApiEchoQuery_NettyServer  thrpt    5  28337.495 ± 1275.362  ops/s
    ServersLatencyBenchmark.graphQLApiEchoQuery_NettyServer      avgt    5    138.122 ±    5.174  us/op
    
    Measuring with connection closing left in the code and moving only the GraphQL server to async processing.
    
    Benchmark                                                    Mode  Cnt      Score     Error  Units
    ServersThroughputBenchmark.graphQLApiEchoQuery_NettyServer  thrpt    5  15442.104 ± 673.331  ops/s
    ServersLatencyBenchmark.graphQLApiEchoQuery_NettyServer      avgt    5    257.802 ±  11.021  us/op

--- server/src/main/java/one/edee/oss/http_server_evaulation_test/server/netty/DispatcherHandler.java
+++ server/src/main/java/one/edee/oss/http_server_evaulation_test/server/netty/DispatcherHandler.java
@@ -7,49 +7,50 @@ import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 
+import java.io.Serial;
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.netty.handler.codec.http.HttpHeaderNames.*;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
 import static io.netty.handler.codec.http.HttpHeaderValues.APPLICATION_JSON;
-import static io.netty.handler.codec.http.HttpHeaderValues.CLOSE;
 import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
 
 public class DispatcherHandler extends SimpleChannelInboundHandler<HttpObject> {
 
-    private static final Map<String, PathHandler> handlers = new HashMap<>() {{
-        put("/", new HelloWorldHandler());
-        put("/graphql", new GraphQLHandler());
-    }};
+	private static final Map<String, PathHandler> handlers = new HashMap<>() {
+		@Serial private static final long serialVersionUID = -1086557078624337728L;
+		{
+			put("/", new HelloWorldHandler());
+			put("/graphql", new GraphQLHandler());
+		}
+	};
 
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) {
-        ctx.flush();
-    }
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		ctx.flush();
+	}
 
-    @Override
-    public void channelRead0(ChannelHandlerContext ctx, HttpObject msg) {
-        if (msg instanceof HttpRequest) {
-            final HttpRequest req = (HttpRequest) msg;
-            final PathHandler handler = handlers.get(req.uri());
-            if (handler == null) {
-                FullHttpResponse response = new DefaultFullHttpResponse(req.protocolVersion(), NOT_FOUND);
-                response.headers()
-                        .set(CONTENT_TYPE, APPLICATION_JSON)
-                        .setInt(CONTENT_LENGTH, response.content().readableBytes());
-                // Tell the client we're going to close the connection.
-                response.headers().set(CONNECTION, CLOSE);
-                ctx.write(response);
-                return;
-            }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
 
-            handler.channelRead(ctx, msg);
-        }
-    }
+	@Override
+	public void channelRead0(ChannelHandlerContext ctx, HttpObject msg) {
+		if (msg instanceof HttpRequest req) {
+			final PathHandler handler = handlers.get(req.uri());
+			if (handler == null) {
+				FullHttpResponse response = new DefaultFullHttpResponse(req.protocolVersion(), NOT_FOUND);
+				response.headers()
+					.set(CONTENT_TYPE, APPLICATION_JSON)
+					.setInt(CONTENT_LENGTH, response.content().readableBytes());
+				ctx.write(response);
+				return;
+			}
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
-        ctx.close();
-    }
+			handler.channelRead(ctx, msg);
+		}
+	}
 }
--- server/src/main/java/one/edee/oss/http_server_evaulation_test/server/netty/GraphQLHandler.java
+++ server/src/main/java/one/edee/oss/http_server_evaulation_test/server/netty/GraphQLHandler.java
@@ -3,10 +3,16 @@ package one.edee.oss.http_server_evaulation_test.server.netty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import one.edee.oss.http_server_evaulation_test.graphql.GraphQLManager;
 import one.edee.oss.http_server_evaulation_test.graphql.GraphQLRequest;
 import one.edee.oss.http_server_evaulation_test.graphql.GraphQLResponse;
@@ -14,10 +20,13 @@ import one.edee.oss.http_server_evaulation_test.graphql.GraphQLResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import static io.netty.handler.codec.http.HttpHeaderNames.*;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
 import static io.netty.handler.codec.http.HttpHeaderValues.APPLICATION_JSON;
-import static io.netty.handler.codec.http.HttpHeaderValues.CLOSE;
-import static io.netty.handler.codec.http.HttpResponseStatus.*;
+import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
+import static io.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR;
+import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 
 public class GraphQLHandler implements PathHandler {
 
@@ -26,41 +35,41 @@ public class GraphQLHandler implements PathHandler {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, HttpObject msg) {
-        if (!(msg instanceof FullHttpRequest)) {
+        if (!(msg instanceof HttpRequest request)) {
+            ctx.fireChannelRead(msg);
             return;
         }
 
-        FullHttpRequest request = (FullHttpRequest) msg;
-        HttpResponse response;
-
         if (!request.uri().equals("/graphql")) {
-            response = createResponse(ctx, request, NOT_FOUND, "".getBytes(StandardCharsets.UTF_8));
+            ctx.writeAndFlush(createResponse(ctx, request, NOT_FOUND, "".getBytes(StandardCharsets.UTF_8)));
         } else if (!request.method().equals(HttpMethod.POST)) {
-            response = createResponse(ctx, request, BAD_REQUEST, "".getBytes(StandardCharsets.UTF_8));
+            ctx.writeAndFlush(createResponse(ctx, request, BAD_REQUEST, "".getBytes(StandardCharsets.UTF_8)));
         } else if (!(request instanceof HttpContent)) {
-            response = createResponse(ctx, request, INTERNAL_SERVER_ERROR, "".getBytes(StandardCharsets.UTF_8));
-        } else {
-            try {
-                // translate request
-                byte[] body = new byte[request.content().readableBytes()];
-                request.content().readBytes(body);
-                final GraphQLRequest graphQLRequest = objectMapper.readValue(body, GraphQLRequest.class);
+            ctx.writeAndFlush(createResponse(ctx, request, INTERNAL_SERVER_ERROR, "".getBytes(StandardCharsets.UTF_8)));
+        } else if (request instanceof FullHttpRequest fullHttpRequest) {
+            final byte[] body = new byte[fullHttpRequest.content().readableBytes()];
+            fullHttpRequest.content().readBytes(body);
+            ctx.executor().execute(() -> {
+                HttpResponse response;
+                try {
+                    // translate request
+                    final GraphQLRequest graphQLRequest = objectMapper.readValue(body, GraphQLRequest.class);
 
-                // execute request
-                final GraphQLResponse<Object> graphQLResponse = graphQLManager.execute(graphQLRequest);
+                    // execute request
+                    final GraphQLResponse<Object> graphQLResponse = graphQLManager.execute(graphQLRequest);
 
-                // send response
-                final byte[] json = objectMapper.writeValueAsBytes(graphQLResponse);
-                response = createResponse(ctx, request, OK, json);
-            } catch (JsonProcessingException e) {
-                response = createResponse(ctx, request, INTERNAL_SERVER_ERROR, "".getBytes(StandardCharsets.UTF_8));
-            } catch (IOException e) {
-                response = createResponse(ctx, request, BAD_REQUEST, "".getBytes(StandardCharsets.UTF_8));
-            }
+                    // send response
+                    final byte[] json = objectMapper.writeValueAsBytes(graphQLResponse);
+                    response = createResponse(ctx, fullHttpRequest, OK, json);
+                } catch (JsonProcessingException e) {
+                    response = createResponse(ctx, fullHttpRequest, INTERNAL_SERVER_ERROR, "".getBytes(StandardCharsets.UTF_8));
+                } catch (IOException e) {
+                    response = createResponse(ctx, fullHttpRequest, BAD_REQUEST, "".getBytes(StandardCharsets.UTF_8));
+                }
+                ctx.writeAndFlush(response);
+            });
+
         }
-
-        ChannelFuture f = ctx.write(response);
-        f.addListener(ChannelFutureListener.CLOSE);
     }
 
     private HttpResponse createResponse(ChannelHandlerContext ctx, HttpRequest request, HttpResponseStatus status, byte[] body) {
@@ -74,8 +83,6 @@ public class GraphQLHandler implements PathHandler {
         response.headers()
                 .set(CONTENT_TYPE, APPLICATION_JSON)
                 .setInt(CONTENT_LENGTH, response.content().readableBytes());
-        // Tell the client we're going to close the connection.
-        response.headers().set(CONNECTION, CLOSE);
         return response;
     }
 }
