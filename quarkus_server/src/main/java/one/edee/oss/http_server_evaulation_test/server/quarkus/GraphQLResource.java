37285612e32f3fb1dda8b8c951fc40a5199416b8
Jan Novotný <novotnaci@gmail.com>
2023-06-04 22:29:10 +0200

    fix: corrected Quarkus build

--- quarkus_server/src/main/java/one/edee/oss/http_server_evaulation_test/server/quarkus/GraphQLResource.java
+++ quarkus_server/src/main/java/one/edee/oss/http_server_evaulation_test/server/quarkus/GraphQLResource.java
@@ -1,37 +1,22 @@
 package one.edee.oss.http_server_evaulation_test.server.quarkus;
 
-import io.smallrye.common.annotation.NonBlocking;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import org.eclipse.microprofile.graphql.GraphQLApi;
-import org.eclipse.microprofile.graphql.Query;
+import one.edee.oss.http_server_evaulation_test.graphql.GraphQLManager;
+import one.edee.oss.http_server_evaulation_test.graphql.GraphQLRequest;
+import one.edee.oss.http_server_evaulation_test.graphql.GraphQLResponse;
 
 @Path("/graphql")
-@GraphQLApi
 public class GraphQLResource {
 
+    private final GraphQLManager graphQLManager;
+
+    public GraphQLResource() {
+        this.graphQLManager = new GraphQLManager();
+    }
+
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @NonBlocking
-    @Query
-    public EchoResponse echo(String message) {
-        return new EchoResponse(message);
+    public GraphQLResponse graphQL(GraphQLRequest request) {
+        return graphQLManager.execute(request);
     }
-
-    public static class EchoResponse {
-        private final String message;
-
-        public EchoResponse(String message) {
-            this.message = message;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-    }
-
 }
